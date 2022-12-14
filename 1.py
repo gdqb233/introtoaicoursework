833b08fe31b9b35b52fe0179ace7dc57b20cf8a1
bigwatermelon <115782586+gdqb233@users.noreply.github.com>
2022-12-14 19:43:18 +0000

    Update 1.py
    add nn

--- 1.py
+++ 1.py
@@ -57,3 +57,50 @@ model.fit(X, y)
 
 y_pred = logisticregressionclassifier.predict(X_test)
 print(accuracy_score(y_test, y_pred))
+
+
+'''
+# neural network
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense#, Activation
+
+import numpy as np
+
+def onehot(X):                                 #Onehot
+    T = np.zeros((X.size, 4))
+    for idx, row in enumerate(T):
+        row[X[idx]-1] = 1
+    return T
+
+y = onehot(y)
+
+model = Sequential()
+model.add(Dense(64, input_dim=X.shape[1], activation='relu'))
+model.add(Dense(y.shape[1],activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+kf = KFold(5)
+
+overallaccuracy = 0
+
+for train, test in kf.split(X):
+    X_train = X[train]
+    y_train = y[train]
+    X_test = X[test]
+    y_test = y[test]
+
+    model.fit(X_train,y_train,verbose=2,epochs=1000)
+    pred = model.predict(X_test)
+    pred = np.argmax(pred,axis=1)
+    y_compare = np.argmax(y_test,axis=1) 
+    score = metrics.accuracy_score(y_compare, pred)
+    overallaccuracy = overallaccuracy + score
+    print("Accuracy score: {}".format(score))
+print ('overall accuracy:', overallaccuracy/5)
+
+'''
+
+
